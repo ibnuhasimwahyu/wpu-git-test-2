7aa55a64abff309399bbb93e9930057872e5971d
ibnuhasimwahyu <wwinarto85@gmail.com>
2022-08-12 14:47:33 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus veritatis doloribus ullam commodi nisi praesentium id, optio quis architecto quia inventore ut molestias consequuntur ad quam illum, atque cum amet.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
